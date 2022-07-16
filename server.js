b1f450f9157d78b5f0b0696050dd31c0a2a7490a
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-07-16 13:21:11 -0300

    forms

--- server.js
+++ server.js
@@ -9,24 +9,25 @@ app.get('/', (request, response)=>{
     response.render('index.ejs', {name: "Ramon"})
 })
 
+/* setting up one route*/
+app.get('/login', (request, response)=>{
+    response.render('login.ejs', {name: "Ramon"})
+})
+
+/* setting up one route*/
+app.get('/register', (request, response)=>{
+    response.render('register.ejs')
+})
+
+
 app.listen(3000)
 
 /* http://localhost:3000/ */
 
-
-
-
-
-
-
 /*
-
 https://www.youtube.com/watch?v=ENrzD9HAZK4&ab_channel=Fireship
 12:45
 
 https://www.youtube.com/watch?v=-RCnNyD0L-s&ab_channel=WebDevSimplified
 5:35
-
-
-
 */ 
